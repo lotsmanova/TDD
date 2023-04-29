fb14f3c1ce6bdcb8648a7c73f1476a3ef02dc775
Violetta <lotsmanovavioletta@gmail.com>
2023-04-29 11:05:11 +0300

    fixture test_dicts.py

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,7 +1,13 @@
+import pytest
 from utils import dicts
 
-def test_get_val():
-    assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
-    assert dicts.get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+@pytest.fixture
+def collection_fixture():
+    return {"vcs": "mercurial"}
+
+
+def test_get_val(collection_fixture):
+    assert dicts.get_val(collection_fixture, "vcs") == "mercurial"
+    assert dicts.get_val(collection_fixture, "vcs", "git") == "mercurial"
     assert dicts.get_val({}, "vcs", "git") == "git"
 
